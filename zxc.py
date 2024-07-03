d01ecfd9f95182e7f371de430bdc355a7a1f38a3
Maksimka-png <maksimsan99@mail.ru>
2024-07-03 11:23:01 +0300

    что-то похожее

--- zxc.py
+++ zxc.py
@@ -1,13 +1,16 @@
 import random
-import time
+class unogame:
+    def _init_(self,players):
+        self.playes = players
+        self.current_play = 0
+        self.deck = ['0','1','2','3','4','5','6','7','8','9']
+        self.hand = [[]for _ in range(len(players))]
 
-
-
-color = ('Red', 'Green', 'Blue', 'Yellow')
-rank = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
-ctype = {'0':'number','1':'number','2':'number','3':'number','4':'number','5':'number','6':'number','7':'number','8':'number','9':'number'}
-
-
-
-
-
+    def deal_cards(self):
+        random.shufle(self.deck)
+        for player in self.hand:
+            for _ in range(7):
+                player.append(self.deck.pop())
+    def play_card(self,card):
+        if card in self.hand[self.current_player]:
+            self
